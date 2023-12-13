2dfbe7f90c35c1c2f96db0cea2c019ddd99b25b6
rogerioncosta <rogerioncosta1@gmail.com>
2023-12-13 14:19:51 -0300

    meu primeiro commit do desafio1 da dio

---
+++ index.js
@@ -0,0 +1,23 @@
+let nome = "Hero";
+let qtdeXP = 10001;
+let nivel = "";
+
+if (qtdeXP < 1000) {
+    nivel = "Ferro";
+}else if (qtdeXP > 1000 && qtdeXP <= 2000) {
+    nivel = "Bronze";
+}else if (qtdeXP > 2000 && qtdeXP <= 5000) {
+    nivel = "Prata";
+}else if (qtdeXP > 5000 && qtdeXP <= 7000) {
+    nivel = "Ouro";
+}else if (qtdeXP > 7000 && qtdeXP <= 8000) {
+    nivel = "Platina";
+}else if (qtdeXP > 8000 && qtdeXP <= 9000) {
+    nivel = "Ascendente";
+}else if (qtdeXP > 9000 && qtdeXP <= 10000) {
+    nivel = "Imortal";
+}else if (qtdeXP >= 10001) {
+    nivel = "Radiante";
+}
+
+console.log("O Herói de nome " + nome + " está no nível de " + nivel);
